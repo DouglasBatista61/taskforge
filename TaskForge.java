906471ea6e012b26eea132e1416fa3996df1726f
Douglas Batista <douglas.firmino@run2biz.com>
2026-08-14 18:01:39 -0300

    Desafio aula 1.1

--- TaskForge.java
+++ TaskForge.java
@@ -14,12 +14,18 @@ void main() {
     String nome = IO.readln("Nome da tarefa: ");
     String descricao = IO.readln("Descrição: ");
     int prioridade = Integer.parseInt(IO.readln("Prioridade (1 a 5): "));
+    double horasEstimadas = Double.parseDouble(IO.readln("Horas estimadas: "));
+    String responsavel = IO.readln("Responsável: ");
+    String status = IO.readln("Status: ");
+
 
     IO.println("");
     IO.println("------ TAREFA CRIADA ------");
     IO.println("Tarefa:     " + nome);
     IO.println("Descrição:  " + descricao);
     IO.println("Prioridade: " + prioridade);
-    IO.println("Status:     pendente");
+    IO.println("Horas estimadas: " + horasEstimadas);
+    IO.println("Responsavel: " + responsavel);
+    IO.println("Status: " + status);
     IO.println("---------------------------");
 }
